4e90663e5283e7da363addfb0f94f1bd6af04df2
kennetor <kenneth.toer@gmail.com>
2023-08-13 23:40:49 +0200

    randomize view

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,11 +1,20 @@
 import MediaType from "../components/MediaType";
 import movies from "../components/movies.json";
 
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 const Movies = () => {
+  const moviesData = shuffleArray([...movies]);
   return (
     <div className="p-4">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {movies.map((showId, i) => (
+        {moviesData.map((showId, i) => (
           <div className="py-2" key={i}>
             <div className="rounded-xl bg-slate-400 hover:cursor-pointer px-2 py-2 hover:bg-slate-300">
               <MediaType key={showId} mediaId={showId} mediaType="movie" />
--- src/pages/Tv.jsx
+++ src/pages/Tv.jsx
@@ -1,12 +1,21 @@
 import MediaType from "../components/MediaType";
-
 import tvShows from "../components/tvShows.json";
 
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 const Tv = () => {
+  const tvData = shuffleArray([...tvShows]);
+
   return (
     <div className="p-4">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {tvShows.map((showId, i) => (
+        {tvData.map((showId, i) => (
           <div className="py-2" key={i}>
             <div className="rounded-xl bg-slate-400 hover:cursor-pointer px-2 py-2 hover:bg-slate-300">
               <MediaType key={showId} mediaId={showId} mediaType="tv" />
